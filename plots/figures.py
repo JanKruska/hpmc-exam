14b051893e827055639a804893d2eb49262c6096
Jan Kruska <jan.kruska@rwth-aachen.de>
2022-02-15 16:14:43 +0100

    Updated figures

--- plots/figures.py
+++ plots/figures.py
@@ -5,7 +5,7 @@ import seaborn as sns
 
 sns.set_theme(style="darkgrid")
 # plt.rcParams['text.usetex'] = True
-DATA_DIR = r"/../data/"
+DATA_DIR = r"../data/"
 PLOTS_DIR = r"."
 FIGURE_FORMAT='png'
 
@@ -16,10 +16,16 @@ def read_data(name):
     return pd.read_csv(DATA_DIR+name)
 
 q3 = read_data("q3.csv")
-plt.plot(q3["m"],q3["GFLOPS"])
+plt.plot(q3["m"],q3["GFLOPS"],label="Max GFLOPS")
+plt.plot(q3["m"],q3["GFLOPS_mean"],label="Mean GFLOPS")
+lower_bound = q3["GFLOPS_mean"] - q3["GFLOPS_std"]
+upper_bound = q3["GFLOPS_mean"] + q3["GFLOPS_std"]
+
+plt.fill_between(q3["m"], lower_bound, upper_bound, alpha=.3)
 plt.xlabel("m")
 plt.ylabel("GFLOPS")
 plt.title(r"GFLOPS performed for m in [1e3,1e4]")
+plt.legend()
 savefig("q3")
 plt.show()
 
